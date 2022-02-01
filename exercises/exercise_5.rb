d5c2ed5e4a6b2eb4d431669175b4d72f2cff8b97
Dickson Yu <dickson.yu@email.com>
2022-02-01 15:16:48 -0500

    Adding the exercises

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@store_sum = Store.sum(:annual_revenue)
+@store_average = Store.average(:annual_revenue)
+@store_annual_sales = Store.where("annual_revenue > 1000000").count
+
+puts @store_sum
+puts @store_average
+puts @store_annual_sales
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create([
+  { first_name: "Khurram", last_name: "Virani", hourly_rate: 60 },
+  { first_name: "John", last_name: "Walton", hourly_rate: 60 },
+  { first_name: "Andy", last_name: "Dufresne", hourly_rate: 60 }
+])
+
+@store2.employees.create([
+  { first_name: "Dom", last_name: "Cobb", hourly_rate: 60 },
+  { first_name: "Arthur", last_name: "Levit", hourly_rate: 60 },
+  { first_name: "Robert", last_name: "Fisher", hourly_rate: 60 }
+])
